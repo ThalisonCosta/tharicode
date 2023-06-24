b2702216ded722cd2e55b2f2236db36024689fd8
thalisonCosta <thalisoncosta123@gmail.com>
2023-06-24 15:30:32 -0400

    :art:

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,19 +1,17 @@
 import { useEffect, useMemo, useRef } from "react";
 import { EditorView, basicSetup } from "codemirror";
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
+import { readFile, writeFile } from "../helpers/filesys";
 import { javascript } from "@codemirror/lang-javascript";
 import { markdown } from "@codemirror/lang-markdown";
 import { html } from "@codemirror/lang-html";
 import { css } from "@codemirror/lang-css";
 import { json } from "@codemirror/lang-json";
 import { rust } from "@codemirror/lang-rust";
-import { materialDark } from "cm6-theme-material-dark";
 import { java } from "@codemirror/lang-java"
 import { python } from "@codemirror/lang-python"
-
-
+import { materialDark } from "cm6-theme-material-dark";
 import { getFileObject } from "../storage/file";
-import { readFile, writeFile } from "../helpers/filesys";
 import { useSource } from "../context/SourceContext";
 
 interface Props {
@@ -21,22 +19,22 @@ interface Props {
   active: boolean;
 }
 
-export default function Code({ id, active }: Props) {
+export default function CodeEditor({ id, active }: Props) {
+  const isRendered = useRef(0)
+  const editorId = useMemo(() => uuid(), [])
+  const visible = active ? '' : 'hidden'
+  const editorRef = useRef<EditorView | null>(null)
   const { closeFile } = useSource();
 
-  const isRendered = useRef(0);
-  const editorId = useMemo(() => uuid(), []);
-  const editorRef = useRef<EditorView | null>(null)
-  const visible = active ? '' : 'hidden';
-
-  const updateContent = async (id: string) => {
+  const updateEditorContent = async (id: string) => {
     const file = getFileObject(id);
     const content = await readFile(file.path)
 
-    loadFileContent(content);
+    fillContentInEditor(content)
+
   }
 
-  const loadFileContent = (content: string) => {
+  const fillContentInEditor = (content: string) => {
     const elem = document.getElementById(editorId)
 
     if (elem && isRendered.current === 0) {
@@ -45,10 +43,8 @@ export default function Code({ id, active }: Props) {
         doc: content,
         extensions: [
           basicSetup,
-          html(), css(), javascript({typescript: true, jsx: true}),
-          json(), markdown(), java(),
-          python(), rust(),
-          materialDark,
+          javascript({typescript: true, jsx: true}), markdown(), html(), css(), json(), rust(), python(), java(),
+          materialDark
         ],
         parent: elem
       })
@@ -59,35 +55,34 @@ export default function Code({ id, active }: Props) {
     if (!editorRef.current) return;
 
     const content = editorRef.current.state.doc.toString();
-    const file = getFileObject(id);
+    const file = getFileObject(id)
 
-    writeFile(file.path, content);
+    writeFile(file.path, content)
   }
 
   useEffect(() => {
-    updateContent(id)
+    updateEditorContent(id)
   }, [id])
 
-  return(
-    <main className={`w-full overflow-y-auto ${visible}`} style={{ height: 'calc(100vh - 40px)' }}>
-      <div id={editorId} tabIndex={-1} onKeyUp={(ev) => {
+  return <main className={`w-full overflow-y-auto ${visible}`} style={{ height: 'calc(100vh - 40px)' }}>
+    <div id={editorId} tabIndex={-1} onKeyUp={(ev) => {
       switch (ev.key) {
-        case 's':
-          if (ev.ctrlKey) {
-            ev.preventDefault();
-            ev.stopPropagation();
-            onSave();
-          }
-          break;
-        case 'w':
-          if (ev.ctrlKey) {
-            ev.stopPropagation();
-            closeFile(id);
-          }
-          break;
-      }
-      }}></div>
+      case 's':
+        if (ev.ctrlKey) {
+          ev.preventDefault();
+          ev.stopPropagation();
+          onSave();
+        }
+        break;
+      case 'w':
+        if (ev.ctrlKey) {
+          ev.stopPropagation();
+          closeFile(id);
+        }
+        break;
+    }
+    }}></div>
 
-    </main>
-  )
+  </main>
+
 }
--- src/components/CodeArea.tsx
+++ src/components/CodeArea.tsx
@@ -1,16 +1,16 @@
-import { useSource } from "../context/SourceContext";
-import { IFile } from "../helpers/filesys";
-import { useHorizontalScroll } from "../helpers/horizontalScroll";
-import { getFileObject } from "../storage/file";
-import Code from "./Code";
-import FileIcon from "./FileIcon";
-import PreviewImage from "./PreviewImage";
+import { useSource } from "../context/SourceContext"
+import { IFile } from "../helpers/filesys"
+import { useHorizontalScroll } from "../helpers/horizontalScroll"
+import { getFileObject } from "../storage/file"
+import Code from "./Code"
+import FileIcon from "./FileIcon"
+import PreviewImage from "./PreviewImage"
 
 export default function CodeArea() {
-  const { opened, selected, setSelect, closeFile } = useSource();
+  const { opened, selected, setSelect, closeFile } = useSource()
   const scrollRef = useHorizontalScroll()
-  const onSelecteItem = (id: string) => {
-    setSelect(id);
+  const onSelectItem = (id: string) => {
+    setSelect(id)
   }
 
   const isImage = (name: string) => {
@@ -23,34 +23,28 @@ export default function CodeArea() {
   }
 
   return <div id="code-area" className="w-full h-full">
-    <div ref={scrollRef}  className="code-tab-items flex items-center border-b border-stone-800 divide-x divide-stone-800 overflow-x-auto">
-      {
-        opened.map(item => {
-          const file = getFileObject(item) as IFile;
-          const active = selected === item ? 'bg-darken text-gray-400' : '';
+    <div ref={scrollRef} className="code-tab-items flex items-center border-b border-stone-800 divide-x divide-stone-800 overflow-x-auto">
+      {opened.map(item => {
+        const file = getFileObject(item) as IFile;
+        const active = selected === item ? 'bg-darken text-gray-400' : ''
 
-          return (
-            <div onClick={() => onSelecteItem(file.id)} className={`tab-item shrink-0 px-3 py-1.5 text-gray-500 cursor-pointer hover:text-gray-400 flex items-center gap-2 ${active}`} key={item}>
-              <FileIcon name={file.name} size="sm" />
-              <span>{file.name}</span>
-              <i onClick={(ev) => close(ev, item)} className="ri-close-line hover:text-red-400"></i>
-            </div>
-          )
-        })
-      }
+        return <div onClick={() => onSelectItem(file.id)} className={`tab-item shrink-0 px-3 py-1.5 text-gray-500 cursor-pointer hover:text-gray-400 flex items-center gap-2 ${active}`} key={item}>
+          <FileIcon name={file.name} size="sm" />
+          <span>{file.name}</span>
+          <i onClick={(ev) => close(ev, item)} className="ri-close-line hover:text-red-400"></i>
+        </div>
+      })}
     </div>
-    <div className="code-content">
-      {
-        opened.map(item => {
-          const file = getFileObject(item) as IFile;
-          if (isImage(file.name)) {
-            return <PreviewImage path={file.path} active={item === selected}/>
-          }
+    <div className="code-contents">
+      {opened.map(item => {
+        const file = getFileObject(item) as IFile;
+        if (isImage(file.name)) {
+          return <PreviewImage path={file.path} active={item === selected} />
+        }
 
-          return <Code key={item} id={item} active={item === selected}/>
-        })
-      }
+        return <Code key={item} id={item} active={item===selected} />
+
+        })}
     </div>
   </div>
-
 }
